ae253acb0a88f9e1ef359cac1ff40f66528f3bf5
PJ <pazuzujindrich@gmail.com>
2025-04-28 16:40:52 -0700

    init commit

---
+++ src/main/java/org/bloom/bloomstack/BloomStackApplication.java
@@ -0,0 +1,13 @@
+package org.bloom.bloomstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bloom/bloomstack/BloomStackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bloom.bloomstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
