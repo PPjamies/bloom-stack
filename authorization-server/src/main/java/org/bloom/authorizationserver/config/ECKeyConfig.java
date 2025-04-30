c293205037ed8f469808b7560d8e993c7122052b
aycippo <acippoletti@sciism.com>
2025-04-30 07:06:43 -0700

    fixes "eliptic curve not supported" exception
    adds the Bouncy Castle provider to the Security singleton and instance generation

--- authorization-server/src/main/java/org/bloom/authorizationserver/config/ECKeyConfig.java
+++ authorization-server/src/main/java/org/bloom/authorizationserver/config/ECKeyConfig.java
@@ -1,10 +1,12 @@
 package org.bloom.authorizationserver.config;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.Security;
 import java.security.spec.ECGenParameterSpec;
 
 @Configuration
@@ -12,7 +14,8 @@ public class ECKeyConfig {
 
     @Bean
     public KeyPair ecKeyPair() throws Exception {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC");
+        Security.addProvider(new BouncyCastleProvider());
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("EC", "BC");
         keyPairGenerator.initialize(new ECGenParameterSpec("secp256k1")); // P-256
         return keyPairGenerator.generateKeyPair();
     }
