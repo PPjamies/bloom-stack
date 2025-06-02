91f698b01c0c5015438937c85596897687ec07e7
PJ <pazuzujindrich@gmail.com>
2025-06-02 12:54:20 -0700

    created Client DTO to send back in the RegisterClientResponse.

--- auth-service/src/main/java/org/bloom/authservice/dto/Client.java
+++ auth-service/src/main/java/org/bloom/authservice/dto/Client.java
@@ -8,6 +8,7 @@ import java.util.List;
 @Data
 @Builder
 public class Client {
+    private String name;
     private String clientId;
     private String clientSecret;
     private String tokenEndpoint;
