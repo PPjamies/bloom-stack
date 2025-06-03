a81eb5f8c2f1c1e57b103f7b8603d047154288fb
PJ <pazuzujindrich@gmail.com>
2025-06-03 12:49:29 -0700

    updated authconverter with mandatory null checks on parameters.
    added conversion between RegisterClient to DBClient in authconverter.
    
    created clientservice to register clients.

--- auth-service/src/main/java/org/bloom/authservice/converter/AuthConverter.java
+++ auth-service/src/main/java/org/bloom/authservice/converter/AuthConverter.java
@@ -1,21 +1,80 @@
 package org.bloom.authservice.converter;
 
+import io.micrometer.common.util.StringUtils;
+import jakarta.validation.constraints.NotNull;
 import org.bloom.authservice.dto.AppUser;
 import org.bloom.authservice.repository.jpa.DBAppUser;
+import org.bloom.authservice.repository.jpa.DBClient;
+import org.bloom.authservice.serializer.Oauth2ClientSerializer;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 public class AuthConverter {
 
-    public static AppUser convert(DBAppUser from) {
+    public static AppUser convert(@NotNull DBAppUser from) {
         return AppUser.builder()
                 .username(from.getUsername())
                 .password(from.getPassword())
                 .build();
     }
 
-    public static DBAppUser convert(AppUser from) {
+    public static DBAppUser convert(@NotNull AppUser from) {
         return DBAppUser.builder()
                 .username(from.getUsername())
                 .password(from.getPassword())
                 .build();
     }
+
+    public static DBClient convert(@NotNull Oauth2ClientSerializer serializer, @NotNull RegisteredClient from) {
+        DBClient to = new DBClient();
+        to.setRegisteredClientId(from.getId());
+        to.setName(from.getClientName());
+        to.setClientId(from.getClientId());
+
+        to.setClientAuthenticationMethods(serializer.serializeClientAuthenticationMethods(from.getClientAuthenticationMethods()));
+        to.setAuthorizationGrantTypes(serializer.serializeAuthorizationGrantTypes(from.getAuthorizationGrantTypes()));
+        to.setScopes(serializer.serializeScopes(from.getScopes()));
+
+        if (StringUtils.isNotBlank(from.getClientSecret())) {
+            to.setClientSecret(from.getClientSecret());
+        }
+
+        if (from.getClientSecretExpiresAt() != null) {
+            to.setClientSecretExpiresAt(from.getClientSecretExpiresAt());
+        }
+
+        if (!ObjectUtils.isEmpty(from.getRedirectUris())) {
+            to.setRedirectUris(serializer.serializeRedirectUris(from.getRedirectUris()));
+        }
+
+        if (from.getClientSettings() != null) {
+            to.setClientSettings(serializer.serializeClientSettings(from.getClientSettings()));
+        }
+
+        if (from.getTokenSettings() != null) {
+            to.setTokenSettings(serializer.serializeTokenSettings(from.getTokenSettings()));
+        }
+
+        return to;
+    }
+
+//    public static RegisteredClient convertToRegisterClient(DBClient from) {
+//        Set<ClientAuthenticationMethod> authMethods = parseClientAuthenticationMethods(dbClient.getClientAuthenticationMethods());
+//        Set<AuthorizationGrantType> grantTypes = parseAuthorizationGrantTypes(dbClient.getAuthorizationGrantTypes());
+//        Set<String> scopes = parseScopes(dbClient.getScopes());
+//
+//        return RegisteredClient.withId(from.getRe)
+//                .clientId(dbClient.getClientId())
+//                .clientSecret(dbClient.getClientSecret())
+//                .clientAuthenticationMethods(authMethods)
+//                .authorizationGrantTypes(grantTypes)
+//                .scopes(scopes)
+//                .clientSecretExpiresAt(dbClient.getClientSecretExpiresAt())
+//                .redirectUris(parseRedirectUris(dbClient.getRedirectUris()))
+//                .clientSettings(ClientSettings.parse(dbClient.getClientSettings()))
+//                .tokenSettings(TokenSettings.parse(dbClient.getTokenSettings()))
+//                .build();
+//    }
 }
---
+++ auth-service/src/main/java/org/bloom/authservice/service/ClientService.java
@@ -0,0 +1,72 @@
+package org.bloom.authservice.service;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.RequiredArgsConstructor;
+import org.bloom.authservice.constant.AuthConstants;
+import org.bloom.authservice.converter.AuthConverter;
+import org.bloom.authservice.dto.Client;
+import org.bloom.authservice.repository.ClientRepository;
+import org.bloom.authservice.serializer.Oauth2ClientSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ClientService {
+
+    @Value("${oauth.token.endpoint}")
+    private String tokenEndpoint;
+
+    private final PasswordEncoder passwordEncoder;
+    private final RegisteredClientRepository registeredClientRepository;
+    private final ClientRepository clientRepository;
+
+    public Client registerClient(@NotBlank String name, @NotBlank String clientId, @NotBlank String clientSecret) {
+
+        String registeredClientId = UUID.randomUUID().toString();
+        String encodedClientSecret = passwordEncoder.encode(clientSecret);
+
+        // create spring oauth2 registered client
+        RegisteredClient registeredClient = RegisteredClient
+                .withId(registeredClientId)
+                .clientName(name)
+                .clientId(clientId)
+                .clientSecret(encodedClientSecret)
+                .clientSecretExpiresAt(Instant.now().plusMillis(AuthConstants.CLIENT_SECRET_EXP_MILLIS))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .scope("read")
+                .scope("write")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenTimeToLive(Duration.ofMillis(AuthConstants.ACCESS_TOKEN_EXP_MILLIS))
+                        .refreshTokenTimeToLive(Duration.ofMillis(AuthConstants.REFRESH_TOKEN_EXP_MILLIS))
+                        .build())
+                .build();
+
+        // register the client with spring's authorization server
+        registeredClientRepository.save(registeredClient);
+
+        // save client to our db
+        clientRepository.save(AuthConverter.convert(new Oauth2ClientSerializer(), registeredClient));
+
+        return Client.builder()
+                .name(name)
+                .clientId(clientId)
+                .clientSecret(encodedClientSecret)
+                .tokenEndpoint(tokenEndpoint)
+                .scopes(List.of("read", "write"))
+                .build();
+    }
+}
